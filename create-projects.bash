323136097b8ca02eee84b5a057429806701d6ebf
jma1 <jason.ma@activenetwork.com>
2020-03-17 11:35:46 +0800

    create microservices template

--- create-projects.bash
+++ create-projects.bash
@@ -4,49 +4,49 @@ mkdir microservices
 cd microservices
 
 spring init \
---boot-version=2.1.0.RELEASE \
+--boot-version=2.2.5.RELEASE \
 --build=gradle \
 --java-version=1.8 \
 --packaging=jar \
 --name=product-service \
---package-name=se.magnus.microservices.core.product \
---groupId=se.magnus.microservices.core.product \
+--package-name=com.mageshuo.microservices.core.product \
+--groupId=com.mageshuo.microservices.core.product \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 product-service
 
 spring init \
---boot-version=2.1.0.RELEASE \
+--boot-version=2.2.5.RELEASE \
 --build=gradle \
 --java-version=1.8 \
 --packaging=jar \
 --name=review-service \
---package-name=se.magnus.microservices.core.review \
---groupId=se.magnus.microservices.core.review \
+--package-name=com.mageshuo.microservices.core.review \
+--groupId=com.mageshuo.microservices.core.review \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 review-service
 
 spring init \
---boot-version=2.1.0.RELEASE \
+--boot-version=2.2.5.RELEASE \
 --build=gradle \
 --java-version=1.8 \
 --packaging=jar \
 --name=recommendation-service \
---package-name=se.magnus.microservices.core.recommendation \
---groupId=se.magnus.microservices.core.recommendation \
+--package-name=com.mageshuo.microservices.core.recommendation \
+--groupId=com.mageshuo.microservices.core.recommendation \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 recommendation-service
 
 spring init \
---boot-version=2.1.0.RELEASE \
+--boot-version=2.2.5.RELEASE \
 --build=gradle \
 --java-version=1.8 \
 --packaging=jar \
 --name=product-composite-service \
---package-name=se.magnus.microservices.composite.product \
---groupId=se.magnus.microservices.composite.product \
+--package-name=com.mageshuo.microservices.composite.product \
+--groupId=com.mageshuo.microservices.composite.product \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 product-composite-service
---
+++ doc/steps.md
@@ -0,0 +1,11 @@
+### Basic steps to follow
+
+1. **Create the directory**
+
+    ```bash
+    mkdir services
+    ```
+
+2. **Create the bootstrap file**
+
+
